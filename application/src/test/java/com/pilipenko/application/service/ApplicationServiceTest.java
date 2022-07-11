2d7184ff97227162ac1392176e4dee561799033e
twoero <iipilipenko@mail.ru>
2022-07-11 19:57:09 +0300

    added service test

--- application/src/test/java/com/pilipenko/application/service/ApplicationServiceTest.java
+++ application/src/test/java/com/pilipenko/application/service/ApplicationServiceTest.java
@@ -4,6 +4,7 @@ import com.pilipenko.application.dto.LoanApplicationRequestDTO;
 import com.pilipenko.application.dto.LoanOfferDTO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -23,6 +25,7 @@ class ApplicationServiceTest {
     @Mock
     private RestTemplateService restTemplateService;
 
+
     @InjectMocks
     private ApplicationService applicationService;
 
@@ -30,12 +33,24 @@ class ApplicationServiceTest {
     void createLoanOffers() throws URISyntaxException {
         LoanApplicationRequestDTO loanApplicationRequestDTO = new LoanApplicationRequestDTO();
         List<LoanOfferDTO> loanOfferDTO = new ArrayList<>();
-        Mockito.when(restTemplateService.postToConveyorOffers(loanApplicationRequestDTO)).thenReturn(loanOfferDTO);
+        when(restTemplateService.postToConveyorOffers(loanApplicationRequestDTO)).thenReturn(loanOfferDTO);
         List<LoanOfferDTO> resultFromApplicationService = applicationService.createLoanOffers(loanApplicationRequestDTO);
         assertEquals(loanOfferDTO, resultFromApplicationService);
     }
 
     @Test
-    void setAppliedOffer() {
+    void createLoanOffersThrowsException() throws URISyntaxException{
+        when(restTemplateService.postToConveyorOffers(ArgumentMatchers.any())).thenThrow(new URISyntaxException("", "reason"));
+        List<LoanOfferDTO> resultFromApplicationService = applicationService.createLoanOffers(new LoanApplicationRequestDTO());
+        assertNull(resultFromApplicationService);
     }
+
+    @Test
+    void setAppliedOffer() throws URISyntaxException{
+        LoanOfferDTO loanOfferDTO = new LoanOfferDTO();
+        applicationService.setAppliedOffer(loanOfferDTO);
+        Mockito.verify(restTemplateService, Mockito.times(1)).postWithAppliedLoanOffer(loanOfferDTO);
+    }
+
+
 }
